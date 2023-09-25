ebec6d88b8f57b20de000da5b3b00580327633e1
praveen632 <33284257+praveen632@users.noreply.github.com>
2023-09-25 12:45:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dropdown-close-on-some-clicks-gamr7v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dropdown-close-on-some-clicks-gamr7v)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'my-app',
+  styleUrls: ['app.component.css'],
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  open = false;
+
+  close() {
+    this.open = false;
+  }
+}
